f687605e4a251e0f7619e3a1000ae3475104b38b
Mav <mavericktesini@gmail.com>
2018-04-18 14:04:45 -0400

    Stubbed states list method using cli call

--- lib/flyquick/cli.rb
+++ lib/flyquick/cli.rb
@@ -3,6 +3,17 @@ class FlyQuick::CLI
 
   def call
     puts "Hello, and welcome, to the FlyQuick."
+    list_states
   end
+
+  def list_states
+    #get_states
+    puts "Florida"
+    puts "Georgia"
+    puts "Texas"
+    puts "Other"
+  end
+
+
 
 end #EOC
