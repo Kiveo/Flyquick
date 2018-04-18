8f956084f4c25b2cfd3ebb20fca66571cf60816c
Mav <mavericktesini@gmail.com>
2018-04-18 14:11:41 -0400

    added stub for state menu prompt

--- lib/flyquick/cli.rb
+++ lib/flyquick/cli.rb
@@ -2,18 +2,23 @@
 class FlyQuick::CLI
 
   def call
-    puts "Hello, and welcome, to the FlyQuick."
+    puts "Welcome to FlyQuick. Available State Selections:"
     list_states
+    make_selection
   end
 
   def list_states
     #get_states
+    puts "--"
     puts "Florida"
     puts "Georgia"
     puts "Texas"
     puts "Other"
   end
 
+  def make_selection
+    puts "\nPlease enter the list number of your desired selection."
+  end
 
 
 end #EOC
